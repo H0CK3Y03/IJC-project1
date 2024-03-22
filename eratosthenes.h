b09d95396ba74cb7534ae9f4cb8ca1c5e396c760
Adam Vesely <adamvesely2003@gmail.com>
2024-03-22 23:31:20 +0100

    Finished eratosthenes.h

---
+++ eratosthenes.h
@@ -0,0 +1,13 @@
+// eratosthenes.h
+// Riešenie IJC-DU1, príklad a), 21.03.2024
+// Autor: Adam Veselý, FIT
+// Preložené gcc version 11.4.0
+
+#ifndef ERATOSTHENES_H
+#define ERATOSTHENES_H
+
+#include "bitset.h"
+
+void eratosthenes(bitset_t name);
+
+#endif /* ERATOSTHENES_H */
